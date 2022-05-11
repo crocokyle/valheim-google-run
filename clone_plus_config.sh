62964a271488c73903d8beb93e39310964773eea
Crocokyle <me@kyle-dixon.com>
2022-05-11 07:37:24 -1000

    Create clone_plus_config.sh

---
+++ clone_plus_config.sh
@@ -0,0 +1,2 @@
+sudo rm /home/htsbackupkyle/valheim-server/config/valheimplus/valheim_plus.cfg
+sudo wget https://raw.githubusercontent.com/crocokyle/valheim-google-run/verve/valheim_plus.cfg /home/htsbackupkyle/valheim-server/config/valheimplus/valheim_plus.cfg
