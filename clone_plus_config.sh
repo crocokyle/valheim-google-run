92fd613e70744b0225136dc8d8a03d58e242cb53
Crocokyle <me@kyle-dixon.com>
2022-05-11 07:40:04 -1000

    Update clone_plus_config.sh

--- clone_plus_config.sh
+++ clone_plus_config.sh
@@ -1,2 +1,3 @@
+# Add to crontab: `echo "@reboot /home/htsbackupkyle/clone_plus_config.sh" > sudo crontab -e` 
 sudo rm /home/htsbackupkyle/valheim-server/config/valheimplus/valheim_plus.cfg
 sudo wget https://raw.githubusercontent.com/crocokyle/valheim-google-run/verve/valheim_plus.cfg /home/htsbackupkyle/valheim-server/config/valheimplus/valheim_plus.cfg
